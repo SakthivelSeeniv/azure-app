01de3d6691df71413a916319d52c8f6d71b5f6a4
SakthivelSeeniv <sakthivel.seeniv@tigeranalytics.com>
2024-01-12 20:15:30 +0530

    Web Changes

--- src/authConfig.js
+++ src/authConfig.js
@@ -5,7 +5,7 @@ const config = {
   auth: {
     authority: 'https://login.microsoftonline.com/cc8f81bd-e3d8-4316-98a8-9409458c0d9e',
     clientId: '614403c6-213e-4193-9e9a-81de56ce8312',
-    redirectUri: 'http://localhost:3000'
+    redirectUri: 'https://pbitriggerapp.azurewebsites.net/'
   },
   cache: {
     cacheLocation: "localStorage",
